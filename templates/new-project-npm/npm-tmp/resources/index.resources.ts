cb63d74a80a5a298b6429380fea2dc6e3797176c
shane <95544653+shanecash@users.noreply.github.com>
2024-08-18 10:11:33 -0400

    Add npm-tmp/resources

---
+++ templates/new-project-npm/npm-tmp/resources/index.resources.ts
@@ -0,0 +1,49 @@
+import { D1Database, Fetcher, Rpc } from "@cloudflare/workers-types";
+
+export type D1Bindings<T extends ReadonlyArray<{ readonly binding: string }>> =
+	{
+		[P in T[number]["binding"]]: D1Database
+	};
+
+export type ServiceRpcBindings<
+	TBindings extends ReadonlyArray<{ readonly binding: string }>,
+    TRpc extends Rpc.WorkerEntrypointBranded | undefined = undefined,
+> = {
+	[P in TBindings[number]["binding"]]: TRpc
+};
+
+export type ServiceFetcherBindings<
+	T extends ReadonlyArray<{ readonly binding: string }>,
+> = {
+	[P in T[number]["binding"]]: Fetcher
+};
+
+export type CloudflareD1 = {
+    name: string;
+}
+
+export function cloudflareD1<T extends CloudflareD1>(resource: T): T {
+    return resource;
+}
+
+export type CloudflareWorkerApi = {
+    name: string;
+    db?: Array<{
+        binding: string
+    }>
+}
+
+export function cloudflareWorkerApi<T extends CloudflareWorkerApi>(resource: T): T {
+    return resource;
+}
+
+export type CloudflarePages = {
+    name: string;
+    services?: Array<{
+        binding: string
+    }>
+}
+
+export function cloudflarePages<T extends CloudflarePages>(resource: T): T {
+    return resource;
+}
