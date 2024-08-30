ee50036d5a5d4c593df2b888dc1b1c70b6fab8dd
shane <95544653+shanecash@users.noreply.github.com>
2024-08-30 08:55:05 -0400

    Add create-gas

--- packages/create-gas/index.create-gas.ts
+++ packages/create-gas/index.create-gas.ts
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 import { parseArgs } from "node:util";
+import { input, confirm, select } from "@inquirer/prompts";
+import fs from "node:fs/promises";
+import path from "node:path";
+import { exec } from "node:child_process";
 
 await main();
 
@@ -28,7 +32,7 @@ Options:
 			parsedArgs.positionals.length === 0 &&
 			Object.keys(parsedArgs.values).length === 0
 		) {
-			console.log("create-gas");
+			await create();
 		} else if (parsedArgs.values.help) {
 			console.log(helpMessage);
 		} else {
@@ -36,5 +40,131 @@ Options:
 		}
 	} catch (error) {
 		console.error(error);
+	}
+}
+
+async function runDirInputPrompt() {
+	const res = await input({
+		message: "Enter directory:",
+		required: true,
+		validate: (input) => {
+			if (input.trim() === "") {
+				return "Directory is required.";
+			}
+			return true;
+		},
+	});
+	return res;
+}
+
+async function runEmptyDirPrompt(dir: string) {
+	const res = await select({
+		message: `${dir} is not empty. Empty it?`,
+		choices: [
+			{ name: "No", value: "no" },
+			{ name: "Yes", value: "yes" },
+			{ name: "Cancel", value: "cancel" },
+		],
+	});
+	return res;
+}
+
+async function selectPackageManagerPrompt() {
+	const res = await select({
+		message: "Select package manager:",
+		choices: [{ name: "npm", value: "npm" }],
+	});
+	return res;
+}
+
+async function runInstallDepsPrompt() {
+	const res = await confirm({
+		message: "Install dependencies?",
+	});
+	return res;
+}
+
+async function create() {
+	let loop = false;
+
+	let dir = await runDirInputPrompt();
+
+	const dirExists = await fs
+		.access(dir)
+		.then(() => true)
+		.catch(() => false);
+
+	let emptyDirPromptRes = "";
+
+	let dirContents: string[] = [];
+
+	if (dirExists) {
+		dirContents = await fs.readdir(dir);
+
+		if (dirContents.length > 0) {
+			loop = true;
+
+			while (loop) {
+				emptyDirPromptRes = await runEmptyDirPrompt(dir);
+
+				switch (emptyDirPromptRes) {
+					case "no":
+						dir = await runDirInputPrompt();
+						break;
+					case "yes":
+						loop = false;
+						break;
+					case "cancel":
+						return;
+				}
+			}
+		}
+	}
+
+	const packageManager = await selectPackageManagerPrompt();
+
+	if (!dirExists) {
+		console.log(`Creating directory ${dir}`);
+		await fs.mkdir(dir, { recursive: true });
+	}
+
+	if (dirContents.length > 0) {
+		console.log(`Emptying directory ${dir}`);
+		await fs.rm(dir, { recursive: true, force: true });
+	}
+
+	const templateDir = path.join(
+		path.dirname(path.dirname(new URL(import.meta.url).pathname)),
+		"template",
+	);
+
+	console.log(`Copying template to ${dir}`);
+	await fs.cp(templateDir, dir, { recursive: true });
+
+	await fs.unlink(path.join(dir, "./gas", ".gitkeep"));
+
+	const gasConfig = await fs.readFile(
+		path.join(dir, "./gas.config.json"),
+		"utf-8",
+	);
+	const gasConfigJson = JSON.parse(gasConfig);
+	gasConfigJson.project = path.basename(dir);
+	await fs.writeFile(
+		path.join(dir, "./gas.config.json"),
+		JSON.stringify(gasConfigJson, null, 2),
+	);
+
+	const turboJson = await fs.readFile(path.join(dir, "./turbo.json"), "utf-8");
+	const turboJsonJson = JSON.parse(turboJson);
+	turboJsonJson.extends = undefined;
+	await fs.writeFile(
+		path.join(dir, "./turbo.json"),
+		JSON.stringify(turboJsonJson, null, 2),
+	);
+	const installDeps = await runInstallDepsPrompt();
+
+	if (installDeps) {
+		console.log("Installing dependencies...");
+		await exec(`${packageManager} install`, { cwd: dir });
 	}
 }
--- packages/create-gas/template/npm-tmp/resources/index.resources.ts
+++ packages/create-gas/template/npm-tmp/resources/index.resources.ts
@@ -2,48 +2,50 @@ import { D1Database, Fetcher, Rpc } from "@cloudflare/workers-types";
 
 export type D1Bindings<T extends ReadonlyArray<{ readonly binding: string }>> =
 	{
-		[P in T[number]["binding"]]: D1Database
+		[P in T[number]["binding"]]: D1Database;
 	};
 
 export type ServiceRpcBindings<
 	TBindings extends ReadonlyArray<{ readonly binding: string }>,
-    TRpc extends Rpc.WorkerEntrypointBranded | undefined = undefined,
+	TRpc extends Rpc.WorkerEntrypointBranded | undefined = undefined,
 > = {
-	[P in TBindings[number]["binding"]]: TRpc
+	[P in TBindings[number]["binding"]]: TRpc;
 };
 
 export type ServiceFetcherBindings<
 	T extends ReadonlyArray<{ readonly binding: string }>,
 > = {
-	[P in T[number]["binding"]]: Fetcher
+	[P in T[number]["binding"]]: Fetcher;
 };
 
 export type CloudflareD1 = {
-    name: string;
-}
+	name: string;
+};
 
 export function cloudflareD1<T extends CloudflareD1>(resource: T): T {
-    return resource;
+	return resource;
 }
 
 export type CloudflareWorkerApi = {
-    name: string;
-    db?: Array<{
-        binding: string
-    }>
-}
+	name: string;
+	db?: Array<{
+		binding: string;
+	}>;
+};
 
-export function cloudflareWorkerApi<T extends CloudflareWorkerApi>(resource: T): T {
-    return resource;
+export function cloudflareWorkerApi<T extends CloudflareWorkerApi>(
+	resource: T,
+): T {
+	return resource;
 }
 
 export type CloudflarePages = {
-    name: string;
-    services?: Array<{
-        binding: string
-    }>
-}
+	name: string;
+	services?: Array<{
+		binding: string;
+	}>;
+};
 
 export function cloudflarePages<T extends CloudflarePages>(resource: T): T {
-    return resource;
+	return resource;
 }
