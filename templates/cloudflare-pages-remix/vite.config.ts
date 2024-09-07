cf0199485d1e43e18cf8586d2c8cb6e104e3cea6
shane <95544653+shanecash@users.noreply.github.com>
2024-09-07 11:57:17 -0400

    Remove comment artifact

--- templates/cloudflare-pages-remix/vite.config.ts
+++ templates/cloudflare-pages-remix/vite.config.ts
@@ -3,8 +3,6 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// vite config
-
 export default defineConfig({
 	plugins: [
 		cloudflareRemixDevPlugin(),
