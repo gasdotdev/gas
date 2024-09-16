848db89dcebefda8e8d23a553969463ef38b6203
shane <95544653+shanecash@users.noreply.github.com>
2024-09-16 05:31:09 -0400

    Update Cloudflare Worker Hono template

--- templates/cloudflare-worker-hono/src/index.entity-group.entity.descriptor.ts
+++ templates/cloudflare-worker-hono/src/index.entity-group.entity.descriptor.ts
@@ -1,7 +1,14 @@
 import { cloudflareWorkerApi } from "@gasdotdev/resources";
+import { Hono } from "hono";
 
 export type Env = {};
 
 export const entityGroupEntityDescriptor = cloudflareWorkerApi({
 	name: "ENTITY_GROUP_ENTITY_DESCRIPTOR",
 } as const);
+
+const app = new Hono<{ Bindings: Env }>();
+
+app.get("/", (c) => c.json({ data: "Hello, World!" }));
+
+export default app;
